e930eb9bcb7c87f2d2ecc36319010597f374d855
Corbin Causby <corbincausby@mac.mynetworksettings.com>
2026-08-15 16:13:20 -0400

    Added breakpoints and fluidity in font sizing through breakpoints with clamp() so it doesnt wrap or overflow until it gets to iphone sizing. Fixed margin overrides and fixed widths.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,6 @@ function App() {
 
   return (
     <>
-      <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-      </head>
-
         <header className="header">
           <h1>Hey! My name is Corbin Causby.</h1>
           <h2>I'm a software engineer and I love to build things.</h2>
@@ -32,7 +28,7 @@ function App() {
                     <h3>{project.title}</h3>
                     <img src={project.image} alt={project.description} />
                     <p>{project.description}</p>
-                    <a href={project.link} target="_blank" rel="noopener noreferrer">View Project</a>                    
+                    <a href={project.link} target="_blank" rel="noopener noreferrer">Check it out!</a>                    
                   </article>
                 </li>
               ))}
