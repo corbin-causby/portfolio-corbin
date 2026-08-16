3f9ba9fb7475f72f7037e79347a4b250010ec6c7
Corbin Causby <corbincausby@mac.mynetworksettings.com>
2026-08-16 15:09:17 -0400

    Added a popup saying my links are copied when right clicking one of the links.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
 import './App.css'
 import { projects } from './data.ts'
 
 function App() {
+
+
+  const [menu, setMenu] = useState({ visible: false, x: 0, y: 0 });
+
+  const handleRightClick = (e: { preventDefault: () => void; currentTarget: { href: any; }; pageX: any; pageY: any; }) => {
+    e.preventDefault();
+    const url = e.currentTarget.href;
+
+    navigator.clipboard.writeText(url);
+
+    setMenu({
+      visible: true,
+      x: e.pageX,
+      y: e.pageY,
+    })
+
+    setTimeout(() => {
+      setMenu(prev => ({ ...prev, visible: false }));
+    }, 2000);
+  };
+
+
+
 
   return (
     <>
@@ -11,8 +35,15 @@ function App() {
         </header>
         <main>
             <section className="contact">
-            <p><a href="https://www.linkedin.com/in/corbin-causby-82646220b/" target="_blank" aria-label="Visit myLinkedIn"><i className="fa fa-linkedin"></i></a></p>
-            <p><a href="https://github.com/corbin-causby" target="_blank" aria-label="Visit my GitHub"><i className="fa fa-github"></i></a></p>
+            <p><a href="mailto:corbincausby01@gmail.com" onContextMenu={handleRightClick} aria-label="Send me an email"><i className="fa fa-envelope"></i></a></p>
+            <p><a href="https://www.linkedin.com/in/corbin-causby-82646220b/" target="_blank" onContextMenu={handleRightClick} aria-label="Visit myLinkedIn"><i className="fa fa-linkedin"></i></a></p>
+            <p><a href="https://github.com/corbin-causby" target="_blank" onContextMenu={handleRightClick} aria-label="Visit my GitHub"><i className="fa fa-github"></i></a></p>
+
+            {menu.visible && (
+            <div className="copy-popup" style={{top: menu.y, left: menu.x,}}>
+              Copied!
+            </div>
+      )}
           </section>
           <section className="about-me">
             <h2>About Me</h2>
@@ -25,7 +56,7 @@ function App() {
               {projects.map((project) => (
                 <li key={project.id}>
                   <article className="project">
-                    <h3>{project.title}</h3>
+                    <h2>{project.title}</h2>
                     <img src={project.image} alt={project.description} />
                     <p>{project.description}</p>
                     <a href={project.link} target="_blank" rel="noopener noreferrer">Check it out!</a>                    
